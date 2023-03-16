a025bfbecbd513f806c52740e3698980b792a551
Berkovics Gellért <berkovicsgellert@ktch.hu>
2023-03-16 15:22:43 +0100

    Jasmine browser runner

--- src/app.js
+++ src/app.js
@@ -5,18 +5,33 @@ let calcButton = document.querySelector('#calcButton')
 
 //console.log(baseInput, heightInput, areaInput, calcButton);
 
-calcButton.addEventListener('click', () => {
-    workInput()
-});
+if(calcButton) {
+    calcButton.addEventListener('click', () => {
+        workInput()
+    })
+}
 
 function workInput() {
     let base = Number(baseInput.value)
     let height = Number(heightInput.value)
-    let area = calcTriangle(base, height)
+    let area = calcTriangleArea(base, height)
     areaInput.value = area
     console.log(area)
 }
 
-function calcTriangle(base, height) {
+function calcTriangleArea(base, height) {
+    if (base <= 0 || height <= 0) {
+        throw new Error('Hiba! Az érték nem megfelelő!');
+    }
     return base * height / 2
+}
+
+function checkInput(input) {
+    //Szabályok kifejezések
+    let res = input.match(/^[0-9]+$/);
+    if (res) {
+        return true;
+    }else {
+        return false;
+    }
 }
